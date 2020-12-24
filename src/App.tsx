b86e114584c449699a9de6c5e17291c3dca75b2d
João Vitor <joao@MacBook-Pro-de-Mac-2.local>
2020-12-24 13:50:50 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  h1 {
+    font-size: 20px;
+  }
+`;
