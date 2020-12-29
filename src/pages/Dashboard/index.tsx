1699412ceb94ef7affd632f3d0278471480a4fcf
João Vitor <joao@MacBook-Pro-de-Mac-2.local>
2020-12-29 18:45:42 -0300

    add api connection and add enviroment variables

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { Table, Landing } from './styles';
 
+interface Stock {
+  _id: string;
+  product: {
+    name: string;
+  };
+  quantity: number;
+  price: number;
+  client: {
+    name: string;
+  };
+}
 const Dashboard: React.FC = () => {
-  async function handleDelete(id: number) {
+  const [products, setProducts] = useState<Stock[]>([]);
+
+  useEffect(() => {
+    async function loadStock(): Promise<void> {
+      const response = await api.get('stocks');
+      setProducts(response.data);
+    }
+
+    loadStock();
+  }, []);
+  async function handleDelete(id: string) {
     await api.delete(`stocks/${id}`);
   }
   return (
@@ -20,23 +41,30 @@ const Dashboard: React.FC = () => {
                 <th>Quantity</th>
                 <th>Product</th>
                 <th>Price</th>
+                <th>Client</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1242</td>
-                <td>22</td>
-                <td>300</td>
-                <td>$2,50</td>
-                <td>
-                  <button type="button" onClick={() => handleDelete(1)}>
-                    Excluir
-                  </button>
-                </td>
-                <td>
-                  <button type="button">Select</button>
-                </td>
-              </tr>
+              {products.map(product => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.quantity}</td>
+                  <td>{product.product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.client.name}</td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => handleDelete(product._id)}
+                    >
+                      Excluir
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button">Select</button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </Table>
