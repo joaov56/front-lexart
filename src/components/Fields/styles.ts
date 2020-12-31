645b5c7cf2b4853553b234656967dccbd520b6af
João Vitor <joao@MacBook-Pro-de-Mac-2.local>
2020-12-31 13:54:21 -0300

    project finished

--- src/components/Fields/styles.ts
+++ src/components/Fields/styles.ts
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 
 export const FieldsContainer = styled.div`
   display: flex;
-  /* flex-direction: column-reverse; */
+
   margin: 90px auto;
   width: 700px;
   background: #ffffff;
   border: 1px solid #d3e2e5;
   display: flex;
-  /* flex: 1; */
   flex-direction: column;
+  border-radius: 16px;
+  padding: 20px 0;
 
   button {
     font-size: 18px;
@@ -41,16 +42,8 @@ export const Field = styled.div`
   }
 
   input {
-    /* border-radius: 8px;
-    border: 0.5px solid rgba(0, 0, 0, 0.3);
-    width: 500px;
-    height: 30px;
-    font-size: 16px;
-    background: #c9c9c9;
-    padding: 8px; */
-
     width: 300px;
-    height: 30px;
+    height: 40px;
     background: #f5f8fa;
     border: 1px solid #d3e2e5;
     outline: none;
